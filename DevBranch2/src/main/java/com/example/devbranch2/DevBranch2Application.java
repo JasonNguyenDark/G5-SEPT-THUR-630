4cad3df83f2638387c5a85dec61745b98f0153e8
Guan <82428319+s3895776@users.noreply.github.com>
2022-08-19 22:36:11 +1000

    devBranchOne deleted, devBranchTwo replacement
    had to fix because I brought over a folder from another project instead of creating a folder inside the actual project (mb)

---
+++ DevBranch2/src/main/java/com/example/devbranch2/DevBranch2Application.java
@@ -0,0 +1,13 @@
+package com.example.devbranch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBranch2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevBranch2Application.class, args);
+    }
+
+}
---
+++ DevBranch2/src/test/java/com/example/devbranch2/DevBranch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devbranch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBranch2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
