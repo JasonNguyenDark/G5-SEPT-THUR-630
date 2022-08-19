a2485816ce214f8c9210d9bac26b78270ded3a06
Guan <82428319+s3895776@users.noreply.github.com>
2022-08-19 22:16:32 +1000

    DevBranch springboot
    created a  folder DevBranch, which I'm not sure is correct, going to look into it

---
+++ DevBranch/src/test/java/com/example/devbranch/DevBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devbranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBranchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
