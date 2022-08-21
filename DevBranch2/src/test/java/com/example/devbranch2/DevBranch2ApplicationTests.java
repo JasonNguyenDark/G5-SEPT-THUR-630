28b0e2da90844de762c85bb18d87035d2da7bd8a
Guan <82428319+s3895776@users.noreply.github.com>
2022-08-21 16:10:51 +1000

    Tests
    added some test names

--- DevBranch2/src/test/java/com/example/devbranch2/DevBranch2ApplicationTests.java
+++ DevBranch2/src/test/java/com/example/devbranch2/DevBranch2ApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.devbranch2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevBranch2ApplicationTests {
+    @Autowired
+    private DemoController controller;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
+    @Test
+    void patientSignUp() {
+
+    }
+    @Test
+    void loginPatient() {
+
+
+    }
+
+    @Test
+    void loginDoctor() {
+
+
+    }
+
+    @Test
+    void doctorUpdateSchedule() {
+
+
+    }
+
+    @Test
+    void patientUpdateStatus() {
+
+
+    }
+
 
 }
