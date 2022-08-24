9a7df736c28891792da24edeb0bdadabd89f1b55
[Thong Nguyen s3769688] <s3769688@student.rmit.edu.au>
2022-08-25 00:44:25 +1000

    Local Database

--- DevBranch2/src/main/java/com/example/devbranch2/DemoController.java
+++ DevBranch2/src/main/java/com/example/devbranch2/DemoController.java
@@ -3,6 +3,11 @@ package com.example.devbranch2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 @RestController
 public class DemoController {
 
@@ -19,9 +24,25 @@ public class DemoController {
     }
 
     @GetMapping("/list")
-    public Iterable<Customer> getCustomers() {
-        return customerRepository.findAll();
+    public void getCustomers() {
+        //ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/sept",
+                    "root","Yes");
+
+            System.out.println("Database connected!");
+            Statement stmt=con.createStatement();
+            ResultSet rs = null;
+            rs = stmt.executeQuery("SELECT * FROM `patient`");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+        //return rs;
     }
+    //return customerRepository.findAll();
 
     @GetMapping("/find/{id}")
     public Customer findCustomerById(@PathVariable Integer id) {
