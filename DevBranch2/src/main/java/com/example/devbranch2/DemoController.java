ad16c4048a1bcd91f6afdd183933eaf9071da39f
[Thong Nguyen s3769688] <s3769688@student.rmit.edu.au>
2022-08-25 23:53:10 +1000

    AWS Database

--- DevBranch2/src/main/java/com/example/devbranch2/DemoController.java
+++ DevBranch2/src/main/java/com/example/devbranch2/DemoController.java
@@ -29,15 +29,39 @@ public class DemoController {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://127.0.0.1:3306/sept",
-                    "root","Yes");
+                    "jdbc:mysql://database-1.cavyvvu6hnkp.us-east-1.rds.amazonaws.com:3306/database-1?characterEncoding=utf8",
+                    "admin_jason", "RMIT!1234");
 
             System.out.println("Database connected!");
             Statement stmt=con.createStatement();
             ResultSet rs = null;
-            rs = stmt.executeQuery("SELECT * FROM `patient`");
-            while(rs.next())
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+            rs = stmt.executeQuery("SELECT * FROM patient");
+            while(rs.next()) {
+                System.out.println("Patient list:");
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  "
+                        + rs.getString(3) + " " + rs.getString(4)+ " " + rs.getString(5));
+            }
+
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://database-1.cavyvvu6hnkp.us-east-1.rds.amazonaws.com:3306/database-1?characterEncoding=utf8",
+                    "admin_jason", "RMIT!1234");
+
+            System.out.println("Database connected!");
+            Statement stmt=con.createStatement();
+            ResultSet rs2 = null;
+            rs2 = stmt.executeQuery("SELECT * FROM doctor");;
+            while(rs2.next()) {
+
+                System.out.println("Doctor list:");
+                System.out.println(rs2.getInt(1)+"  "+rs2.getString(2)+"  "
+                        +rs2.getString(3)+" "+rs2.getString(4)+" "+rs2.getString(5));
+            }
+
             con.close();
         }catch(Exception e){ System.out.println(e);}
         //return rs;
