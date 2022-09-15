6427f79216bfe496760c43b4e582508f26ad2ee3
Huili233 <Huili233@hotmail.com>
2022-09-15 15:53:10 +1000

    first commit

---
+++ DevBranch2/src/main/java/com/example/devbranch2/MessageDoctor.java
@@ -0,0 +1,20 @@
+package com.example.devbranch2;
+
+public class MessageDoctor {
+    private String name;
+
+    public void HelloMessage() {
+    }
+
+    public void HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
