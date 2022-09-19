e2c5235758cd9ae3ab3e8362419e951f48437873
Guan <82428319+s3895776@users.noreply.github.com>
2022-09-19 19:55:11 +1000

    Microservice websockets
    implement the websocket as a microservice

---
+++ SpringWebSockets/src/main/java/com/example/springwebsockets/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.springwebsockets;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // should remain as gs-guide-websocket (will not connect without a specific
+        // path)
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+    }
+
+}
