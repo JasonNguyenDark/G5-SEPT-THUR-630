866c8e5254ef9f231d5e400d245ec7d8a7d5174a
s3662687 <s3662687@student.rmit.edu.au>
2022-09-20 21:24:08 +1000

    misc

---
+++ ND_Telemedicine/lib/Globals/appbar.dart
@@ -0,0 +1,162 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:nd_telemedicine/Globals/variables.dart';
+
+// For landing page and sign up
+class DefaultTopNav extends StatelessWidget implements PreferredSizeWidget {
+  const DefaultTopNav({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        'NY TELEMEDICINE',
+
+        //all styling goes here
+        style: TextStyle(
+          fontFamily: 'Arvo',
+          color: Colors.black,
+          fontSize: 36.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+// For screens after login
+class GlobalNavBar extends StatelessWidget implements PreferredSizeWidget {
+  GlobalNavBar({Key? key}) : super(key: key);
+
+  final TextEditingController roleController = TextEditingController(text: "");
+
+  // Read values
+  Future<void> readFromStorage() async {
+    roleController.text = await credentialStorage.read(key: "Key_role") ?? '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+        future: readFromStorage(),
+        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+          return AppBar(
+            actions: <Widget> [
+
+              //HOME
+              DecoratedBox(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      left: BorderSide(color: Colors.black),
+                      right: BorderSide(color: Colors.black),
+                    ),
+                  ),
+
+                child: TextButton(
+                  child: itemName("HOME"),
+                  onPressed: () {},
+                ),
+              ),
+
+              //PROFILE
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  border: rightBorder(),
+                ),
+
+                child: TextButton(
+                  child: itemName("PROFILE"),
+                  onPressed: () {},
+                ),
+              ),
+
+              
+              //If login user is a patient they can use booking
+              //if login user is a doctor they have schedule instead
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  border: rightBorder(),
+                ),
+
+                child: TextButton(
+                  child: itemName(specificName("BOOKING", "SCHEDULE", roleController.text)),
+                  onPressed: () {},
+                ),
+              ),
+
+              //If login user is a patient they can view health record
+              //if login user is a doctor they can prescribe medicine
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  border: rightBorder(),
+                ),
+
+                child: TextButton(
+                  child: itemName(specificName("RECORD", "PRESCRIBE", roleController.text)),
+                  onPressed: () {
+                    if(roleController.text == "user") {
+                      Navigator.pushNamed(context, '/record');
+                    }
+                  },
+                ),
+              ),
+
+              // CHAT
+              // same design regardless if patient or doctor but different routing
+              DecoratedBox(
+                decoration: BoxDecoration(
+                  border: rightBorder(),
+                ),
+
+                child: TextButton(
+                  child: itemName("CHAT"),
+                  onPressed: () {
+                    //example
+                    // if(roleController.text == "doctor") {
+                    //   Navigator.pushNamed(context, 'doctorChat');
+                    // } else {
+                    //   Navigator.pushNamed(context, 'userChat');
+                    // }
+                  },
+                ),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+Text itemName(String name) {
+  return Text(
+    name,
+
+    style: const TextStyle(
+      fontFamily: 'Arvo',
+      color: Colors.black,
+      fontSize: 16.0,
+    ),
+  );
+}
+
+Border rightBorder() {
+  return const Border(
+    right: BorderSide(color: Colors.black),
+  );
+}
+
+String specificName(String userItem, String doctorItem, String role) {
+  if(role == 'doctor') {
+    return doctorItem;
+  } else {
+    return userItem;
+  }
+}
