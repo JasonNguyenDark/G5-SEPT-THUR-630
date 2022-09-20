856454dedece6989253825b1bfeace02fd244138
[Thong Nguyen s3769688] <s3769688@student.rmit.edu.au>
2022-09-20 22:34:13 +1000

    Chatting feature -Jason

--- ND_Telemedicine/lib/Screens/chat.dart
+++ ND_Telemedicine/lib/Screens/chat.dart
@@ -1,22 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:nd_telemedicine/Globals/variables.dart';
+import 'package:nd_telemedicine/Models/Record.dart';
 
+import 'package:flutter/material.dart';
+import 'package:nd_telemedicine/Screens/record.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:pusher_channels_flutter/pusher_channels_flutter.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+import 'package:dash_chat_2/dash_chat_2.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+// Header
+class ChatPage extends StatefulWidget {
+  const ChatPage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => HomePageState();
+  State<ChatPage> createState() => ChatPageState();
 }
 
-class HomePageState extends State<HomePage> {
-
+class ChatPageState extends State<ChatPage> {
   final TextEditingController roleController = TextEditingController(text: "");
 
   // Read values
   Future<void> readFromStorage() async {
     roleController.text = await credentialStorage.read(key: "Key_role") ?? '';
-
   }
 
   @override
@@ -26,67 +37,67 @@ class HomePageState extends State<HomePage> {
 
       //Top bar/header
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+          backgroundColor: Colors.blue,
 
-        title: FutureBuilder<void> (
-          future: readFromStorage(),
-          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-            return Text(
-              roleController.text,
+          title: FutureBuilder<void> (
+            future: readFromStorage(),
+            builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+              return Text(
+                roleController.text,
 
-              //all styling goes here
-              style: const TextStyle(
-                fontFamily: 'Arvo',
-                color: Colors.black,
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
-            );
-          },
-        ),
+                //all styling goes here
+                style: const TextStyle(
+                  fontFamily: 'Arvo',
+                  color: Colors.black,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              );
+            },
+          ),
 
           actions: <Widget>[
-              //home
-              DecoratedBox(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    left: BorderSide(color: Colors.black),
-                    right: BorderSide(color: Colors.black),
-                  ),
+            //home
+            DecoratedBox(
+              decoration: const BoxDecoration(
+                border: Border(
+                  left: BorderSide(color: Colors.black),
+                  right: BorderSide(color: Colors.black),
                 ),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Home',
-                    style: TextStyle(
-                      fontFamily: 'Arvo',
-                      color: Colors.black,
-                      fontSize: 16.0,
-                    ),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Home',
+                  style: TextStyle(
+                    fontFamily: 'Arvo',
+                    color: Colors.black,
+                    fontSize: 16.0,
                   ),
                 ),
               ),
+            ),
 
-              // profile
-              DecoratedBox(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    left: BorderSide(color: Colors.black),
-                    right: BorderSide(color: Colors.black),
-                  ),
+            // profile
+            DecoratedBox(
+              decoration: const BoxDecoration(
+                border: Border(
+                  left: BorderSide(color: Colors.black),
+                  right: BorderSide(color: Colors.black),
                 ),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                      'profile',
-                    style: TextStyle(
-                      fontFamily: 'Arvo',
-                      color: Colors.black,
-                      fontSize: 16.0,
-                    ),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'profile',
+                  style: TextStyle(
+                    fontFamily: 'Arvo',
+                    color: Colors.black,
+                    fontSize: 16.0,
                   ),
                 ),
               ),
+            ),
 
             // Booking/Scheduling
             DecoratedBox(
@@ -154,7 +165,7 @@ class HomePageState extends State<HomePage> {
                         );
                       } else {
                         return TextButton(
-                          onPressed: () {Navigator.pushNamed(context, '/record');},
+                          onPressed: () {},
                           child: const Text(
                             'Record',
                             style: TextStyle(
@@ -167,7 +178,6 @@ class HomePageState extends State<HomePage> {
                       }
                     })),
 
-            // Changed here --Jason
             // Chat
             DecoratedBox(
               decoration: const BoxDecoration(
@@ -177,7 +187,7 @@ class HomePageState extends State<HomePage> {
                 ),
               ),
               child: TextButton(
-                onPressed: () {Navigator.pushNamed(context, '/chat');},
+                onPressed: () {},
                 child: const Text(
                   'Chat',
                   style: TextStyle(
@@ -191,6 +201,7 @@ class HomePageState extends State<HomePage> {
           ]
       ),
 
+      body: const Content(),
       //Footer
       bottomNavigationBar: const Footer(),
 
@@ -198,7 +209,57 @@ class HomePageState extends State<HomePage> {
   }
 }
 
+// Body
+class Basic extends StatefulWidget {
+  @override
+  _BasicState createState() => _BasicState();
+}
 
+class _BasicState extends State<Basic> {
+  ChatUser user = ChatUser(
+    id: '1',
+    firstName: 'Charles',
+    lastName: 'Leclerc',
+  );
+
+  List<ChatMessage> messages = <ChatMessage>[
+    ChatMessage(
+      text: 'Hey!',
+      user: user,
+      createdAt: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basic example'),
+      ),
+      body: DashChat(
+        currentUser: user,
+        onSend: (ChatMessage m) {
+          setState(() {
+            messages.insert(0, m);
+          });
+        },
+        messages: messages,
+      ),
+    );
+  }
+}
+
+class ChatMessage {
+}
+
+class ChatUser {
+  String? id;
+  String? profileImage;
+  Map<String, dynamic>? customProperties; String? firstName; 
+  String? lastName;
+}
+
+// Footer
 class Footer extends StatefulWidget {
   const Footer({Key? key}) : super(key: key);
 
