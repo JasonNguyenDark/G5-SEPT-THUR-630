2584a0d135b5240c68d44c3bffe0e4cc6966738d
Guan <82428319+s3895776@users.noreply.github.com>
2022-09-25 11:06:15 +1000

    Update BackEndApplication.java

--- BackEnd/src/main/java/com/example/backend/BackEndApplication.java
+++ BackEnd/src/main/java/com/example/backend/BackEndApplication.java
@@ -1,14 +1,9 @@
 package com.example.backend;
 
-import com.example.backend.controller.FormController;
-import com.example.backend.controller.RecordController;
-import com.example.backend.controller.ScheduleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {FormController.class, RecordController.class, ScheduleController.class})
 public class BackEndApplication {
 
     public static void main(String[] args) {
